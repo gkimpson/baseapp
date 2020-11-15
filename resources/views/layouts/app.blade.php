199e96dc0373a16ba73aa5ad136c70240ccab3aa
Gavin Kimpson <gkimpson@gmail.com>
2020-11-15 11:24:00 +0000

    comment out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,14 +45,14 @@
                         <nav class="mt-5 px-2">
                             <a href="/"
                                class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150">
-                                @if($photo)
-                                    <svg
-                                        class="mr-4 h-6 w-6 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
-                                    </svg>
-                                @endif
+{{--                                @if($photo)--}}
+{{--                                    <svg--}}
+{{--                                        class="mr-4 h-6 w-6 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150"--}}
+{{--                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">--}}
+{{--                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>--}}
+{{--                                    </svg>--}}
+{{--                                @endif--}}
                                 Dashboard
                             </a>
                             <a href="/team"
